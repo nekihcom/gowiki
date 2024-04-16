34942484015aaf85339b9f70850cc96811fd71ba
nekihcom <kmmch.tech@gmail.com>
2024-04-16 23:04:15 +0900

    Using net/http to serve wiki pages

--- wiki.go
+++ wiki.go
@@ -1,7 +1,11 @@
+//go:build ignore
+
 package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -11,7 +15,7 @@ type Page struct {
 }
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt" // ファイル名
+	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
@@ -24,9 +28,13 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
